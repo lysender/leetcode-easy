4bfd288b808eafdbcefb285164e19a5f77f46abf
Lysender <lysender@darkstar-win>
2024-02-08 09:19:27 +0800

    fix: add feb 08

--- src/bin/b20240207.rs
+++ src/bin/b20240207.rs
@@ -2,6 +2,7 @@ struct Solution {}
 
 fn main() {
     Solution::count_asterisks("".to_string());
+    Solution::has_alternating_bits(5);
 }
 
 impl Solution {
@@ -24,6 +25,24 @@ impl Solution {
         }
         count
     }
+
+    pub fn has_alternating_bits(n: i32) -> bool {
+        // Convert to binary then string?
+        // Or use some bit shifting logic (I don't know bit shifting!)
+        // Will just use string and be done with it
+        let bin_s = format!("{:b}", n);
+        let mut prev: Option<char> = None;
+        for ch in bin_s.chars() {
+            if let Some(prev_val) = prev {
+                if prev_val == ch {
+                    return false;
+                }
+            }
+            prev = Some(ch);
+        }
+        // println!("{:?}", bin_s);
+        true
+    }
 }
 
 #[cfg(test)]
@@ -35,5 +54,12 @@ mod tests {
         assert_eq!(Solution::count_asterisks("l|*e*et|c**o|*de|".to_string()), 2);
         assert_eq!(Solution::count_asterisks("iamprogrammer".to_string()), 0);
         assert_eq!(Solution::count_asterisks("yo|uar|e**|b|e***au|tifu|l".to_string()), 5);
+    }
+
+    #[test]
+    fn test_alternating_bits() {
+        assert_eq!(Solution::has_alternating_bits(5), true);
+        assert_eq!(Solution::has_alternating_bits(7), false);
+        assert_eq!(Solution::has_alternating_bits(11), false);
     }
 }
