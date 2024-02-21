bcc9bc0ff4740ad764b2ffc6299c8a4205750119
Lysender <lysender@darkstar-win>
2024-02-21 10:30:48 +0800

    fix: add most visited sector solution

--- src/bin/b20240220.rs
+++ src/bin/b20240220.rs
@@ -3,6 +3,7 @@ struct Solution {}
 fn main() {
     Solution::prefix_count(vec![], "at".to_string());
     Solution::distribute_candies(7, 3);
+    Solution::most_visited(4, vec![1, 3, 1, 2]);
 }
 
 impl Solution {
@@ -36,11 +37,74 @@ impl Solution {
 
         answer
     }
+
+    pub fn most_visited(n: i32, rounds: Vec<i32>) -> Vec<i32> {
+        let mut visits: Vec<i32> = vec![0; n as usize];
+        let mut prev: Option<usize> = None;
+
+        for (k, v) in rounds.iter().enumerate().skip(1) {
+            let start = rounds[k - 1] as usize;
+            let end = rounds[k] as usize;
+            let mut current = start;
+
+            println!("k: {}, n: {}", k, v);
+
+
+            // Round it up until we reach end
+            loop {
+                println!("current: {}", current);
+                // Mark as visited but only if it was not previously visited
+                let mut is_visited = false;
+                if let Some(prev_val) = prev {
+                    is_visited = prev_val == current;
+                }
+
+                if !is_visited {
+                    visits[current - 1] += 1;
+                }
+
+                prev = Some(current);
+
+                // Did we reach the end?
+                if current == end {
+                    break;
+                }
+
+                // Still need to run
+                if current == n as usize {
+                    // Reset to first sector
+                    current = 1;
+                } else {
+                    current += 1;
+                }
+            }
+        }
+
+        println!("visits: {:?}", visits);
+        // Find the most visited item
+        let max = visits.iter().max();
+        if let Some(max_val) = max {
+            return visits
+                .iter()
+                .enumerate()
+                .filter(|(_k, v)| *v == max_val)
+                .map(|(k, _v)| (k as i32) + 1)
+                .collect();
+        }
+        vec![] 
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_most_visited() {
+        assert_eq!(Solution::most_visited(4, vec![1, 3, 1, 2]), vec![1, 2]);
+        assert_eq!(Solution::most_visited(2, vec![2,1,2,1,2,1,2,1,2]), vec![2]);
+        assert_eq!(Solution::most_visited(7, vec![1,3,5,7]), vec![1,2,3,4,5,6,7]);
+    }
 
     #[test]
     fn test_distribute_candies() {
